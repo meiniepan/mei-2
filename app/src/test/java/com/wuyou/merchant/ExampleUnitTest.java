4a9783c555fe9bf038101bd5c5be7d3b04b09678
wangboning <meiniepan@163.com>
2018-05-31 11:17:03 +0800

    provider 冲突，删除冲突的xml文件

--- app/src/test/java/com/wuyou/merchant/ExampleUnitTest.java
+++ app/src/test/java/com/wuyou/merchant/ExampleUnitTest.java
@@ -1,10 +1,10 @@
 package com.wuyou.merchant;
 
-import android.util.Log;
+import com.wuyou.merchant.util.EncodeUtil;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -17,8 +17,9 @@ public class ExampleUnitTest {
         assertEquals(4, 2 + 2);
     }
     @Test
-    public void aa(){
+    public void aa() throws Exception {
         assertEquals(4, 2 + 2);
-        System.out.println("22");
+        String a = EncodeUtil.get3DES("aaa","1AE48E613CD877D041A21E6F383C7FE682F6C7EC");
+        System.out.println(a);
     }
 }
